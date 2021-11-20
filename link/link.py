2c0ad180e504b07f06467fad9ad4a5af621123d0
JoshSelvey <selvey.josh@gmail.com>
2021-11-20 09:56:18 +0000

    Removed link class

--- link/link.py
+++ link/link.py
@@ -1,53 +1,6 @@
 import pandas as pd
 
 
-class Link:
-
-  def __init__(self, team, player1, player2):
-    self.player1 = player1
-    self.player2 = player2
-    self.player_to_player = pd.read_csv(f'Data/Player_to_Player/{team}.csv', dtype=str).set_index('player').to_dict()['players']
-    self.player_to_player = {player: players.split(',') for player, players in self.player_to_player.items()}
-    self.player_info = pd.read_csv(f'Data/Team_to_Player/{team}.csv', dtype=str).set_index('id').to_dict()['name']
-    self.players = {self.player1}
-    self.dag, self.numbered = self.bfs()
-    self.paths(self.dag, self.player2)
-    self.dag = {p: [a for a in self.dag[p] if a in self.players] for p in self.players}
-    self.data = {
-      'nodes': [{'id': s, 'group': self.numbered[s], 'name': self.player_info[s]} for s in self.dag],
-      'links': [{"source": s, "target": e, "value": 1} for s in self.dag for e in self.dag[s]]
-      }
-
-  def bfs(self):
-    parent = {self.player1: [self.player1]}
-    queue = [self.player1]
-    visited = {self.player1: 1}
-    while queue:
-      current_node = queue.pop(0)
-      if current_node == self.player2:
-        return parent, visited
-      for node in self.player_to_player[current_node]:
-        if node not in visited:
-          visited[node] = visited[current_node] + 1
-          queue.append(node)
-          parent[node] = [current_node]
-        elif visited[current_node] == visited[parent[node][0]]:
-          parent[node].append(current_node)
-    return False, {}
-
-  def paths(self, dag, end):
-    if end == self.player1:
-      return [end]
-    result = []
-    for e in dag[end]:
-      subpaths = self.paths(dag, e)
-      for subpath in subpaths:
-        result.append(f'{end},{subpaths}')
-        self.players.add(end)
-    return result
-
-#####
-
 def get_link(team, player1, player2):
   player_to_player = pd.read_csv(f'Data/Player_to_Player/{team}.csv', dtype=str).set_index('player').to_dict()['players']
   player_to_player = {player: players.split(',') for player, players in player_to_player.items()}

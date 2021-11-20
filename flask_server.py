3b89e930b6193129b6d853fc1b05f89dc2e3c02d
JoshSelvey <selvey.josh@gmail.com>
2021-11-20 16:44:51 +0000

    Set up gunicorn and Docker.

--- flask_server.py
+++ flask_server.py
@@ -4,7 +4,12 @@ from flask import Flask
 
 from link.link import get_link
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./build', static_url_path='/')
+
+
+@app.route('/')
+def index():
+  return app.send_static_file('index.html')
 
 
 @app.route('/api/get_player_dropdown')
